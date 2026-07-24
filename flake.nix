7b47ce61853af1ac262fff7c337e4d391c532c84
Christopher Mühl <toki@toph.so>
2026-07-24 16:20:13 +0200

    chore: scaffold sparql-pod crate

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "sparql-pod - SPARQL-authoritative Solid pod";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            clang
+            libclang
+            pkg-config
+            openssl
+          ];
+          shellHook = ''
+            export LIBCLANG_PATH="${pkgs.libclang.lib}/lib"
+          '';
+        };
+      }
+    );
+}
